a65e0766474a9cb800f5067858c3facca282e609
Winicius Girardi <wgirardi541@gmail.com>
2024-01-25 11:02:09 -0300

    feat:Exception for validation and entity not found

---
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/exception/ProductValidationException.java
@@ -0,0 +1,9 @@
+package com.compassuol.sp.challenge.ecommerce.exception;
+
+public class ProductValidationException extends RuntimeException{
+
+    public ProductValidationException(String message){
+        super(message);
+    }
+
+}
