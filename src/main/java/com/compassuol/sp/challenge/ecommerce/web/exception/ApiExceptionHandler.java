67ec8a626db9ec36db98384b4023a23dcc4f76f1
Winicius Girardi <wgirardi541@gmail.com>
2024-01-26 13:34:23 -0300

    fix: added treatment for request bad formatted

--- src/main/java/com/compassuol/sp/challenge/ecommerce/web/exception/ApiExceptionHandler.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/web/exception/ApiExceptionHandler.java
@@ -9,12 +9,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 
 
 @Slf4j
@@ -24,66 +24,59 @@ public class ApiExceptionHandler {
     @ExceptionHandler({MethodArgumentNotValidException.class})
     public ResponseEntity<ErrorMessage> methodArgumentNotValidException(MethodArgumentNotValidException ex,
                                                                         HttpServletRequest request,
-                                                                        BindingResult result){
-
-        log.error("Api Error - ", ex);
-        return ResponseEntity
-                .status(HttpStatus.UNPROCESSABLE_ENTITY)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(new ErrorMessage(request,HttpStatus.UNPROCESSABLE_ENTITY,"Campo(s) invalidos",result));
-    }
-
-    @ExceptionHandler({ProductValidationException.class, DuplicateProductNameException.class})
-    public ResponseEntity<ErrorMessage> dataIntegrityViolationException(RuntimeException ex,
-                                                                        HttpServletRequest request
-                                                                        ){
-
-        log.error("Api Error - ", ex);
-        return ResponseEntity
-                .status(HttpStatus.CONFLICT)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(new ErrorMessage(request,HttpStatus.CONFLICT,ex.getMessage()));
-
-    }
-
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ErrorMessage> entityNotFoundException(RuntimeException ex,
-                                                                        HttpServletRequest request
-    ){
-
-        log.error("Api Error - ", ex);
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(new ErrorMessage(request,HttpStatus.NOT_FOUND, ex.getMessage()));
-
-    }
-    @ExceptionHandler(org.springframework.http.converter.HttpMessageNotReadableException.class)
-    public ResponseEntity<ErrorMessage> httpMessageNotReadableException(RuntimeException ex,HttpServletRequest request){
+                                                                        BindingResult result) {
 
         log.error("Api Error - ", ex);
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(new ErrorMessage(request,HttpStatus.BAD_REQUEST, "Requisição inválida"));
+                .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, "Campo(s) invalidos", result));
+    }
+
+    @ExceptionHandler({ProductValidationException.class, DuplicateProductNameException.class})
+    public ResponseEntity<ErrorMessage> dataIntegrityViolationException(RuntimeException ex,
+                                                                        HttpServletRequest request
+    ) {
+
+        log.error("Api Error - ", ex);
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.CONFLICT, ex.getMessage()));
 
     }
+
+    @ExceptionHandler({EntityNotFoundException.class, org.springframework.web.servlet.resource.NoResourceFoundException.class})
+    public ResponseEntity<ErrorMessage> entityNotFoundException(RuntimeException ex,
+                                                                HttpServletRequest request
+    ) {
+
+        log.error("Api Error - ", ex);
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.NOT_FOUND, ex.getMessage()));
+
+    }
+
+
+
     @ExceptionHandler(org.springframework.web.HttpRequestMethodNotSupportedException.class)
-    public ResponseEntity<ErrorMessage> httpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex, HttpServletRequest request){
+    public ResponseEntity<ErrorMessage> httpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex, HttpServletRequest request) {
         log.error("Api Error - ", ex);
         return ResponseEntity
                 .status(HttpStatus.METHOD_NOT_ALLOWED)
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(new ErrorMessage(request,HttpStatus.METHOD_NOT_ALLOWED, "Método não permitido para esta rota"));
+                .body(new ErrorMessage(request, HttpStatus.METHOD_NOT_ALLOWED, "Método não permitido para esta rota"));
     }
 
-
-
-
-
-
-
-
-
+    @ExceptionHandler(org.springframework.http.converter.HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorMessage> httpMessageNotReadableException(HttpMessageNotReadableException ex, HttpServletRequest request) {
+        log.error("Api Error - ", ex);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, "Requisição incompleta ou mal formatada"));
+    }
 
 }
