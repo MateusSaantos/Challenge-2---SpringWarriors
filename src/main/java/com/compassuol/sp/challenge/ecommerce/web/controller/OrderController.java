aa5a94a18714aee28516550e32b0ffdbfd0c030c
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-01 14:04:02 -0300

    feat: added getAll and update endpoints

--- src/main/java/com/compassuol/sp/challenge/ecommerce/web/controller/OrderController.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/web/controller/OrderController.java
@@ -5,14 +5,15 @@ import com.compassuol.sp.challenge.ecommerce.entities.Order;
 import com.compassuol.sp.challenge.ecommerce.services.OrderService;
 import com.compassuol.sp.challenge.ecommerce.web.dto.mapper.OrderMapper;
 import com.compassuol.sp.challenge.ecommerce.web.dto.order.OrderResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -22,9 +23,21 @@ public class OrderController {
 
     private final OrderService orderService;
 
+    @GetMapping
+    public ResponseEntity<List<OrderResponseDto>> getAll(@RequestParam(name = "status", required = false) String status){
+        List<Order> orders = orderService.getAll(status);
+        return ResponseEntity.ok(OrderMapper.toListDto(orders));
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<OrderResponseDto> getById(@PathVariable Long id) {
         Order order = orderService.getById(id);
         return ResponseEntity.status(HttpStatus.OK).body(OrderMapper.toDto(order));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<OrderResponseDto> update(@PathVariable Long id, @RequestBody Order updateOrder){
+        Order order = orderService.update(id, updateOrder);
+        return ResponseEntity.ok(OrderMapper.toDto(order));
     }
 }
