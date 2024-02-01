b2f7b4d75b42b4bc0b670c858127f9cc71950223
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-01 14:04:29 -0300

    feat: added toListDto method:

--- src/main/java/com/compassuol/sp/challenge/ecommerce/web/dto/mapper/OrderMapper.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/web/dto/mapper/OrderMapper.java
@@ -1,9 +1,13 @@
 package com.compassuol.sp.challenge.ecommerce.web.dto.mapper;
 
 import com.compassuol.sp.challenge.ecommerce.entities.Order;
+import com.compassuol.sp.challenge.ecommerce.entities.Product;
 import com.compassuol.sp.challenge.ecommerce.web.dto.order.OrderCreateDto;
 import com.compassuol.sp.challenge.ecommerce.web.dto.order.OrderResponseDto;
 import org.modelmapper.ModelMapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class OrderMapper {
@@ -20,6 +24,8 @@ public class OrderMapper {
         return modelMapper.map(order, OrderResponseDto.class);
         }
 
-
+    public static List<OrderResponseDto> toListDto(List<Order> orders) {
+        return orders.stream().map(OrderMapper::toDto).collect(Collectors.toList());
     }
+}
 
