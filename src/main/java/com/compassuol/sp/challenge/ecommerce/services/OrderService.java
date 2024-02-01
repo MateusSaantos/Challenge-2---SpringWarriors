8d05f8ce07bc0093331476447d3382ca1289005d
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-01 14:03:50 -0300

    feat: added getAll and update services

--- src/main/java/com/compassuol/sp/challenge/ecommerce/services/OrderService.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/services/OrderService.java
@@ -5,8 +5,11 @@ import com.compassuol.sp.challenge.ecommerce.entities.Order;
 import com.compassuol.sp.challenge.ecommerce.exception.EntityNotFoundException;
 import com.compassuol.sp.challenge.ecommerce.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -14,9 +17,26 @@ public class OrderService {
     private final OrderRepository orderRepository;
 
     @Transactional(readOnly = true)
+    public List<Order> getAll(String status){
+        Sort sortByCreatedAtDesc = Sort.by(Sort.Order.desc("createdAt"));
+        if (!status.isEmpty()){
+            return orderRepository.findAllByStatus(Order.Status.valueOf(status), sortByCreatedAtDesc);
+        } else {
+            return orderRepository.findAll(sortByCreatedAtDesc);
+        }
+    }
+
+    @Transactional(readOnly = true)
     public Order getById(Long id){
         return orderRepository.findById(id).orElseThrow(
                 () -> new EntityNotFoundException(String.format("Order id=%s não encontrado",id)));
+    }
+
+    @Transactional
+    public Order update(Long id, Order updatedOrder){
+        Order existingOrder = getById(id);
+        existingOrder.setStatus(updatedOrder.getStatus());
+        return orderRepository.save(existingOrder);
     }
 
 
